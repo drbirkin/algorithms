b4afdbb9dfef9fa85fae24d475911d3f1aa1bbea
David Y <d.yang@pinkelephant.com>
2023-08-29 09:59:21 -0400

    add recursion

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import time
 
-def linear_search (value, arr, N):
-    for i in range(N):
-        if value == arr[i]:
-            return i
-    return False
 
-start = time.perf_counter()
+def linear_search(value, arr, index):
+    for i in range(index):
+        if value == arr[i]:
+            return {"index": i, "is_found": True}
+    return {}
+
+
+def linear_search_recursive(value, arr, index):
+    if index < 0:
+        return {"is_found": False}
+    elif arr[index] == value:
+        return {"index": index, "is_found": True}
+    else:
+        return linear_search_recursive(value, arr, index - 1)
+
+
 
 arr = [2, 3, 7, 10, 12]
 target = 2
 index_n = len(arr)
-index = linear_search(target, arr, index_n)
+index = index_n if index_n < len(arr) else index_n - 1
+
+start = time.perf_counter()
+
+result = linear_search(target, arr, index)
 
 end = time.perf_counter()
+print(f"{end - start} secs")
 
-print(f'{end - start} secs')
+start = time.perf_counter()
 
-if index or index == 0:
-    print(f'{target} found at index {index}')
+result_recursive = linear_search_recursive(target, arr, index)
+
+end = time.perf_counter()
+print(f"{end - start} secs")
+
+if result.get('is_found', False):
+    print(f"{target} found at index {result.get('index', None)}")
 else:
-    print('Element not found')
-    
+    print("Element not found")
+
+print(
+    f"At {result_recursive.get('index', None) if result_recursive.get('is_found', False) else None} is {target} found"
+)
