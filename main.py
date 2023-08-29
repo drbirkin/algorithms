aa124df642199458646e53f8eb2112c67ab47634
David Y <d.yang@pinkelephant.com>
2023-08-29 13:30:50 -0400

    add sentinel linear search

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 
 
 def linear_search(value, arr, index):
-    for i in range(index):
+    for i in range(index + 1):
         if value == arr[i]:
             return {"index": i, "is_found": True}
     return {}
@@ -17,12 +17,29 @@ def linear_search_recursive(value, arr, index):
         return linear_search_recursive(value, arr, index - 1)
 
 
+def linear_search_sentinel(value, arr):
+    last_index = len(arr) - 1
+    last_element = arr[last_index]
+    arr[last_index] = value
 
-arr = [2, 3, 7, 10, 12]
-target = 2
+    i = 0
+    while arr[i] != value:
+        i += 1
+
+    arr[last_index] = last_element
+
+    if i < last_index or arr[last_index] == value:
+        return {"index": i, "is_found": True}
+    else:
+        return {"is_found": False}
+
+
+target = 120000
+arr = list(range(target + 1))
 index_n = len(arr)
 index = index_n if index_n < len(arr) else index_n - 1
 
+# linear search
 start = time.perf_counter()
 
 result = linear_search(target, arr, index)
@@ -30,6 +47,11 @@ result = linear_search(target, arr, index)
 end = time.perf_counter()
 print(f"{end - start} secs")
 
+print(
+    f"At {result.get('index', None) if result.get('is_found', False) else None} is {target} found"
+)
+
+# recursive linear search
 start = time.perf_counter()
 
 result_recursive = linear_search_recursive(target, arr, index)
@@ -37,11 +59,18 @@ result_recursive = linear_search_recursive(target, arr, index)
 end = time.perf_counter()
 print(f"{end - start} secs")
 
-if result.get('is_found', False):
-    print(f"{target} found at index {result.get('index', None)}")
-else:
-    print("Element not found")
+print(
+    f" (recursive) At {result_recursive.get('index', None) if result_recursive.get('is_found', False) else None} is {target} found"
+)
+
+# sentinel linear search
+start = time.perf_counter()
+
+result_sentinel = linear_search_sentinel(target, arr)
+
+end = time.perf_counter()
+print(f"{end - start} secs")
 
 print(
-    f"At {result_recursive.get('index', None) if result_recursive.get('is_found', False) else None} is {target} found"
+    f" (sentinel) At {result_sentinel.get('index', None) if result_sentinel.get('is_found', False) else None} is {target} found"
 )
