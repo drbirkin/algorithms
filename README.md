f55c49e784d09ee89b2e05917559f72653e0bab1
David Y <d.yang@pinkelephant.com>
2024-01-02 16:41:19 -0500

    quick union add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # algorithms
+
+union find is slow worst case would be n^2 not scalble 
--- main.py
+++ main.py
@@ -19,8 +19,41 @@ class Unionfind:
         for id in range(len(self.array)): 
             if self.array[id] == p_value:
                 self.array[id] = q_value
+class Quickunion:
+    array = list()
     
-new_find = Unionfind(10)
-new_find.union(2,3)
-print(new_find.is_connect(2,3))
-print(new_find.array)
+    def __init__(self, N:int):
+        self.__length = N
+        self.array = list(range(self.__length + 1))
+        
+    def root(self, value:int):
+        while value != self.array[value]:
+            value = self.array[value]
+        return value
+    
+    def is_connect (self, p:int, q:int):
+        return self.root(p) == self.root(q)
+    
+    def union (self, p:int, q:int):
+        p_value = self.root(p)
+        q_value = self.root(q)
+        
+        for id in range(len(self.array)): 
+            if self.array[id] == p_value:
+                self.array[id] = q_value
+
+# Union find 
+# time complexity n^2
+union_find = Unionfind(10)
+union_find.union(2,3)
+print(union_find.is_connect(2,3))
+print(union_find.array)
+
+# Quick union
+# time complexity n
+quick_union = Quickunion(10)
+quick_union.union(2,4)
+print(quick_union.is_connect(2,4))
+print(quick_union.array)
+
+# TODO improvements with weigthed QU and path comparision also both
