63c1db5d0cf3b4d6178594218290400d080f0c7b
david <davidliat98@gmail.com>
2024-01-10 07:46:13 -0500

    update

--- interview_questions.py
+++ interview_questions.py
@@ -3,12 +3,12 @@ from typing import List
 import math
 
 # Question 1
-# Social network connectivity. Given a social network containing 
-# n members and a log file containing 
-# m timestamps at which times pairs of members formed friendships, TODO: design an algorithm to determine the earliest time 
-# at which all members are connected (i.e., every member is a friend of a friend of a friend ... of a friend). 
-# Assume that the log file is sorted by timestamp and that friendship is an equivalence relation. The running time of your algorithm should be 
-# mlogn or better and use extra space proportional to 
+# Social network connectivity. Given a social network containing
+# n members and a log file containing
+# m timestamps at which times pairs of members formed friendships, TODO: design an algorithm to determine the earliest time
+# at which all members are connected (i.e., every member is a friend of a friend of a friend ... of a friend).
+# Assume that the log file is sorted by timestamp and that friendship is an equivalence relation. The running time of your algorithm should be
+# mlogn or better and use extra space proportional to
 # n.
 
 class Socialmedia:
@@ -16,11 +16,18 @@ class Socialmedia:
     def __init__(self, N:int):
         self.__length = N
         self.array = [{"name": f"name-{i}",
+                       "friend": f'name-{i}',
                        "id": i} for i in range(self.__length + 1)]
-    # O(n) 
+    # O(logn)
     def is_connect(self, p_name:str, q_name:str):
         p_id = p_name.split('-')[1]
         q_id = q_name.split('-')[1]
-                
-        return p_id == q_id
-    
+
+        p_friend = self.root[p_id]
+        q_friend = self.root[q_id]
+
+        return p_friend.get("friend") == q_friend.get("friend")
+    # O(logn)
+    def root(self, id:int):
+        id = self.array[self.root(self.array[id])]
+        return id
