faed098caa10d3f621098d0de4477426acdf3115
David Y <d.yang@pinkelephant.com>
2024-09-11 12:13:56 -0400

    deep first search

--- main.py
+++ main.py
@@ -2,127 +2,48 @@ import time
 from typing import List
 import math
 
-class Unionfind:
-    array = list()
-    # O(n)
-    def __init__(self, N:int):
-        self.__length = N
-        self.array = list(range(self.__length + 1))
-    # O(1)
-    def is_connect (self, p:int, q:int):
-        return self.array[p] == self.array[q]
+class Graph:
+    def __init__(self, num_of_nodes, directed=True):
+        self.m_num_of_nodes = num_of_nodes
+        self.m_nodes = range(self.m_num_of_nodes)
 
-    # O(n)
-    def union (self, p_id:int, q_id:int):
-        p_value = self.array[p_id]
-        q_value = self.array[q_id]
-        # Change all entries with p to q
-        for id in range(len(self.array)):
-            if self.array[id] == p_value:
-                self.array[id] = q_value
+        # Define the type of a graph
+        self.m_directed = directed
 
-class Quickunion:
-    array = list()
-    # O(n)
-    def __init__(self, N:int):
-        self.__length = N
-        self.array = list(range(self.__length + 1))
-    # O(n)
-    def root(self, value:int):
-        while value != self.array[value]:
-            value = self.array[value]
-        return value
-    # O(n)
-    def is_connect (self, p:int, q:int):
-        return self.root(p) == self.root(q)
-    # O(n)
-    def union (self, p:int, q:int):
-        p_value = self.root(p)
-        q_value = self.root(q)
-        # Change root value
-        self.array[p_value] = self.array[q_value]
+        self.m_adj_list = {node: set() for node in self.m_nodes}      
 
-class Weight_QU:
-    array = list()
-    # O(n)
-    def __init__(self, N:int):
-        self.__length = N
-        self.array = list(range(self.__length + 1))
-        self.size = [1] * N
-    # O(log(n)) in amortized time analysis, but not witth n-1 deep tree (O(n))
-    def root(self, value:int):
-        while value != self.array[value]:
-            value = self.array[value]
-        return value
-    # O(log(n))
-    def is_connect (self, p:int, q:int):
-        return self.root(p) == self.root(q)
-    # O(log(n))
-    def union (self, p:int, q:int):
-        p_value = self.root(p)
-        q_value = self.root(q)
+    def add_edge(self, node1, node2, weight=1):
+        self.m_adj_list[node1].add((node2, weight))
+        
+        if not self.m_directed:
+            self.m_adj_list[node2].add((node1, weight))
+         
+    def dfs(self, start, target, path = [], visited = set()):
+        path.append(start)
+        visited.add(start)
+        if start == target:
+            return path
+        for (neighbour, weight) in self.m_adj_list[start]:
+            if neighbour not in visited:
+                result = self.dfs(neighbour, target, path, visited)
+                if result is not None:
+                    return result
+        path.pop()
+        return None   
+    
+    def print_adj_list(self):
+        for key in self.m_adj_list.keys():
+            print("node", key, ": ", self.m_adj_list[key])
 
-        if p_value != q_value:
+graph = Graph(5)
 
-            if self.size[p_value] <= self.size[q_value]:
-                self.array[p_value] = q_value
-                self.size[q_value] += self.size[p_value]
-            else:
-                self.array[q_value] = p_value
-                self.size[p_value] += self.size[q_value]
+graph.add_edge(0, 0, 25)
+graph.add_edge(0, 1, 5)
+graph.add_edge(0, 2, 3)
+graph.add_edge(1, 3, 1)
+graph.add_edge(1, 4, 15)
+graph.add_edge(4, 2, 7)
+graph.add_edge(4, 3, 11)
 
-class Weight_Path_QU:
-    array = list()
-    # O(n)
-    def __init__(self, N:int):
-        self.__length = N
-        self.array = list(range(self.__length + 1))
-        self.size = [1] * N
-    # O(log(n)) in amortized time analysis, but not witth n-1 deep tree (O(n))
-    def root(self, value:int):
-        if value != self.array[value]:
-        # Path compression
-            value = self.root(self.array[value])
-        return value
-    # O(log(n))
-    def is_connect (self, p:int, q:int):
-        return self.root(p) == self.root(q)
-    # O(log(n))
-    def union (self, p:int, q:int):
-        p_value = self.root(p)
-        q_value = self.root(q)
-
-        if p_value != q_value:
-
-            if self.size[p_value] <= self.size[q_value]:
-                self.array[p_value] = q_value
-                self.size[q_value] += self.size[p_value]
-            else:
-                self.array[q_value] = p_value
-                self.size[p_value] += self.size[q_value]
-# Union find
-# time complexity n^2 on n operations (n * union operations)
-union_find = Unionfind(10)
-union_find.union(2,3)
-print(union_find.array)
-print(union_find.is_connect(2,3))
-
-# Quick union
-# time complexity n^2 on n operations but should be slightly performant
-quick_union = Quickunion(10)
-quick_union.union(2,4)
-print(quick_union.array)
-print(quick_union.is_connect(2,4))
-
-# Weighted Quick union
-# time complexity n*log(n) on n operations
-weight_qu = Weight_QU(10)
-weight_qu.union(2,4)
-print(weight_qu.array)
-weight_qu.union(1,6)
-print(weight_qu.array)
-weight_qu.union(6,8)
-print(weight_qu.array)
-print(weight_qu.is_connect(1,8))
-
-# TODO improvements with weigthed QU and path comparision also both
+graph.print_adj_list()
+print(graph.dfs(0, 3))
